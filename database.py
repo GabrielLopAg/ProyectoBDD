52d6d857e535060bbe13a3a64b1f1b529230e7de
Gabriel Lopez <gla280900@gmail.com>
2022-12-02 00:40:19 -0600

    comment print

--- database.py
+++ database.py
@@ -5,15 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 server = os.getenv('SERVER')
-database = os.getenv('DATABASE') 
+database = os.getenv('DATABASE')
 username = os.getenv('USERNAME')
 password = os.getenv('PASSWORD')
 
 
 def connect():
     try:
-        connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes;')
-        print("Conexión exitosa.")
+        connection = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes;')
+        # print("Conexión exitosa.")
         cursor = connection.cursor()
         return cursor
     except Exception as ex:
